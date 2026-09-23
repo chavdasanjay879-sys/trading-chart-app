5028b9ad9975917dfb2285ad1ea3988f17cffcc7
chavdasanjay879-sys <chavda.sanjay879@gmail.com>
2026-09-23 15:05:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 import yfinance as yf
 
 app = FastAPI()
 
-PAGE = """@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def home():
-return PAGE
+    return FileResponse("index.html")
 
 @app.get("/api/candles/{symbol}")
 def get_candles(symbol: str):
-ticker = yf.Ticker(symbol)
-df = ticker.history(period="1mo", interval="1d")
-candles = []
-for idx, row in df.iterrows():
-candles.append({
-"time": idx.strftime("%Y-%m-%d"),
-"open": round(float(row["Open"]), 2),
-"high": round(float(row["High"]), 2),
-"low": round(float(row["Low"]), 2),
-"close": round(float(row["Close"]), 2)
-})
-return candles
+    ticker = yf.Ticker(symbol)
+    df = ticker.history(period="1mo", interval="1d")
+    candles = []
+    for idx, row in df.iterrows():
+        candles.append({
+            "time": idx.strftime("%Y-%m-%d"),
+            "open": round(float(row["Open"]), 2),
+            "high": round(float(row["High"]), 2),
+            "low": round(float(row["Low"]), 2),
+            "close": round(float(row["Close"]), 2)
+        })
+    return candles
